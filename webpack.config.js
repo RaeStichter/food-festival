dccf1ea2383866bba3eb0aa21e611f1ad1c2c2f2
Rae Stichter <raestichter@gmail.com>
2021-03-15 13:26:14 -0400

    bundles

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const webpack = require("webpack");
+
+// this is where the entry for the bunldle is and the resulting file.
+module.exports = {
+    entry: './assets/js/script.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+    ],
+    mode: 'development'
+};
